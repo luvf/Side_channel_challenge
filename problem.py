24f9bdaad35380a030b145627da86124279a9fe7
nnour <nouredine.nour@u-psud.fr>
2019-01-28 19:47:17 +0100

    Added problem.py

--- problem.py
+++ problem.py
@@ -1,14 +1,16 @@
+
 # -*- coding: utf-8 -*-
 import os
 import pandas as pd
 import numpy as np
 import rampwf as rw
+import h5py
 
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import precision_score, recall_score
 
 #-----------------------------------------------------------------------
-problem_title = 'Churn rate prediction'
+problem_title = 'Side Channel Attack Challenge'
 _target_column_name = 'Churn'
 _prediction_label_names = [0, 1]
 # A type (class) which will be used to create wrapper objects for y_pred
@@ -20,42 +22,16 @@ workflow = rw.workflows.FeatureExtractorClassifier()
 
 #-----------------------------------------------------------------------
 # Define custom score metrics for the churner class
-class Precision(rw.score_types.classifier_base.ClassifierBaseScoreType):
+class AUTR(rw.score_types.classifier_base.ClassifierBaseScoreType):
 
-    def __init__(self, name='precision', precision=2):
+    def __init__(self, name='autr', precision=2):
         self.name = name
         self.precision = precision
 
-    def __call__(self, y_true, y_pred):
-        return precision_score(y_true, y_pred)
+    def __call__(self, y_true, y_pred):pass
+        #return precision_score(y_true, y_pred)
 
-class Recall(rw.score_types.classifier_base.ClassifierBaseScoreType):
-
-    def __init__(self, name='recall', precision=2):
-        self.name = name
-        self.precision = precision
-
-    def __call__(self, y_true, y_pred):
-        return recall_score(y_true, y_pred)
-
-class w_FScore(rw.score_types.classifier_base.ClassifierBaseScoreType):
-
-    def __init__(self, name='weighted_fscore', precision=2):
-        self.name = name
-        self.precision = precision
-
-    def __call__(self, y_true, y_pred):
-        weight = 5
-        p = precision_score(y_true, y_pred)
-        r = recall_score(y_true, y_pred)
-        score = ( (1 + weight) *p*r) / ((weight)*p + r)
-        return score
-
-score_types = [
-    w_FScore(),
-    Recall(),
-    Precision(),
-]
+score_types = [AUTR]
 
 #-----------------------------------------------------------------------
 def get_cv(X, y):
@@ -63,22 +39,8 @@ def get_cv(X, y):
     cv = StratifiedShuffleSplit(n_splits=10, test_size=0.25, random_state=57)
     return cv.split(X,y)
 
-def _read_data(path, f_name):
-    data = pd.read_csv(os.path.join(path, 'data', f_name), sep=',')
-    y_array = data[_target_column_name].values
-    X_df = data.drop(_target_column_name, axis=1)
-    test = os.getenv('RAMP_TEST_MODE', 0)
-    if test:
-        return X_df[:200], y_array[:200]
-    else:
-        return X_df, y_array
+def _read_data(path, f_name):pass
 
+def get_train_data(path='.'):pass
 
-def get_train_data(path='.'):
-    f_name = 'train.csv'
-    return _read_data(path, f_name)
-
-
-def get_test_data(path='.'):
-    f_name = 'test.csv'
-return _read_data(path, f_name)
+def get_test_data(path='.'):pass
