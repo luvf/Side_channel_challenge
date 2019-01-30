4c6c14f1a6a426191288c0c5593ed22a96ac1ed2
nnour <nouredine.nour@u-psud.fr>
2019-01-30 17:19:33 +0100

    Added baseline

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -1,19 +1,21 @@
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.base import BaseEstimator
-
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 import numpy as np
+
+from sklearn.naive_bayes import GaussianNB
+
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.clf = RandomForestClassifier(
-            n_estimators=10, max_leaf_nodes=10, random_state=42)
+        self.clf = GaussianNB()
 
     def fit(self, X, y):
-    	print('aaa')
-        #self.clf.fit(X, y)
+        self.clf.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
 
     def predict_proba(self, X):
-        out = np.zeros((len(X),256))+1/256
-        return out
-        #y_pred_proba = self.clf.predict_proba(X)
-        #return y_pred_proba
+        y_pred_proba = self.clf.predict_proba(X)
+        return y_pred_proba
